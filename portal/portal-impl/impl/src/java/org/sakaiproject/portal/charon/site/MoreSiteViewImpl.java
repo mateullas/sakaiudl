josecebe/sakai

MSVINT-26 Canvis portal1 - Especific UdL

portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/site/MoreSiteViewImpl.java
@@ -267,6 +267,15 @@ public class MoreSiteViewImpl extends AbstractSiteViewImpl
 		//SAK-30712
 		String[] moresitesExternalSites = serverConfigurationService.getStrings("moresites.externalConfig.siteTypes");
 		String moresitesExternalPrefix = serverConfigurationService.getString("moresites.externalConfig.prefix","moresites_");
+		String visibleTerms = serverConfigurationService.getString("portal.term.visibleTerms");
+		List <String> visibleTermTabs = new ArrayList<String> ();
+
+		String [] termTabs = visibleTerms.split (","); 
+
+		if (termTabs != null){
+			visibleTermTabs.addAll(Arrays.asList(termTabs));
+		}
+
 		boolean moresitesExternalConfig = (moresitesExternalSites!=null) && (moresitesExternalSites.length>0);
 		
 		Map<String, String> moresitesExternalSiteTypes = new HashMap<String, String>();
@@ -295,8 +304,11 @@ public class MoreSiteViewImpl extends AbstractSiteViewImpl
 				term = siteProperties.getProperty("term");
 				if(null==term) {
 					term = rb.getString("moresite_unknown_term");
+				} else if ("9999-99".equals (term)){
+					term = rb.getString("moresite_doctorat");
+				} else if (!visibleTermTabs.contains(term)){
+					term = rb.getString ("moresite_other_terms");
 				}
-
 			}
 			else if (isProjectType(type))
 			{
@@ -315,14 +327,18 @@ public class MoreSiteViewImpl extends AbstractSiteViewImpl
 				term = rb.getString("moresite_other");
 			}
 
-			List<Site> currentList = new ArrayList();
-			if (termsToSites.containsKey(term))
-			{
-				currentList = termsToSites.get(term);
-				termsToSites.remove(term);
+
+			if (!"hidden".equals (type)){
+				List<Site> currentList = new ArrayList();
+				if (termsToSites.containsKey(term))
+				{
+					currentList = termsToSites.get(term);
+					termsToSites.remove(term);
+				}
+				currentList.add(site);
+				termsToSites.put(term, currentList);
 			}
-			currentList.add(site);
-			termsToSites.put(term, currentList);
+			
 		}
 
 		class TitleSorter implements Comparator<Map>
@@ -388,9 +404,13 @@ public class MoreSiteViewImpl extends AbstractSiteViewImpl
 			result.sitesInRightPane.put(term, new ArrayList());
 
 			for (Map site : (List<Map>)tabsMoreTerms.get(term)) {
-				if (isCourseType((String)site.get("siteType"))) {
+				String siteType = (String)site.get("siteType");
+
+				if (isCourseType(siteType)) {
 					result.sitesInLeftPane.get(term).add(site);
-				} else {
+				} else if (	"projectedocent".equals(siteType) ||
+							"coord".equals (siteType)) {				
+						result.sitesInLeftPane.get(term).add(site);
 					result.sitesInRightPane.get(term).add(site);
 				}
 			}

mateullas/sakaiudl

portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/site/MoreSiteViewImpl.java
@@ -233,6 +233,15 @@ public class MoreSiteViewImpl extends AbstractSiteViewImpl
 		//SAK-30712
 		String[] moresitesExternalSites = serverConfigurationService.getStrings("moresites.externalConfig.siteTypes");
 		String moresitesExternalPrefix = serverConfigurationService.getString("moresites.externalConfig.prefix","moresites_");
+		String visibleTerms = serverConfigurationService.getString("portal.term.visibleTerms");
+		List <String> visibleTermTabs = new ArrayList<String> ();
+
+		String [] termTabs = visibleTerms.split (","); 
+
+		if (termTabs != null){
+			visibleTermTabs.addAll(Arrays.asList(termTabs));
+		}
+
 		boolean moresitesExternalConfig = (moresitesExternalSites!=null) && (moresitesExternalSites.length>0);
 		
 		Map<String, String> moresitesExternalSiteTypes = new HashMap<String, String>();
@@ -261,8 +270,11 @@ public class MoreSiteViewImpl extends AbstractSiteViewImpl
 				term = siteProperties.getProperty("term");
 				if(null==term) {
 					term = rb.getString("moresite_unknown_term");
+				} else if ("9999-99".equals (term)){
+					term = rb.getString("moresite_doctorat");
+				} else if (!visibleTermTabs.contains(term)){
+					term = rb.getString ("moresite_other_terms");
 				}
-
 			}
 			else if (isProjectType(type))
 			{
@@ -277,14 +289,18 @@ public class MoreSiteViewImpl extends AbstractSiteViewImpl
 				term = rb.getString("moresite_other");
 			}
 
-			List<Site> currentList = new ArrayList();
-			if (termsToSites.containsKey(term))
-			{
-				currentList = termsToSites.get(term);
-				termsToSites.remove(term);
+
+			if (!"hidden".equals (type)){
+				List<Site> currentList = new ArrayList();
+				if (termsToSites.containsKey(term))
+				{
+					currentList = termsToSites.get(term);
+					termsToSites.remove(term);
+				}
+				currentList.add(site);
+				termsToSites.put(term, currentList);
 			}
-			currentList.add(site);
-			termsToSites.put(term, currentList);
+			
 		}
 
 		class TitleSorter implements Comparator<Map>
@@ -350,7 +366,11 @@ public class MoreSiteViewImpl extends AbstractSiteViewImpl
 						.orElse(null);
 				if (isCourseType(type)) {
 					result.sitesInLeftPane.get(term).add(site);
-				} else {
+				} else if (	"projectedocent".equals(type) ||
+							"coord".equals (type)) {				
+						result.sitesInLeftPane.get(term).add(site);
+				}
+				else {
 					result.sitesInRightPane.get(term).add(site);
 				}
 			}